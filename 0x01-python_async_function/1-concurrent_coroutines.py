aa68948149e2c311ff610c4ce2c9b9ef9df8eeec
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2024-10-17 20:40:09 +0100

    concurrent coroutines

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Asynchronous Random Delay Module.
+
+This module provides `wait_n`, that spawn wait_random n times
+ with the specified max_delay.
+
+"""
+
+from asyncio import sleep
+
+
+async def wait_n(n: int, max_delay: int) -> list:
+    """
+    Spawns wait_random n times with the specified max_delay.
+
+    Args:
+        n (int): Number of times to spawn wait_random.
+        max_delay (int): The maximum delay for each wait_random call.
+
+    Returns:
+        list: A list of the delays in ascending order.
+    """
+    delays = []
+    wait_random = __import__('0-basic_async_syntax').wait_random
+    for _ in range(n):
+        delay = await wait_random(max_delay)
+        inserted = False
+        for i in range(len(delays)):
+            if delay < delays[i]:
+                delays.insert(i, delay)
+                inserted = True
+                break
+        if not inserted:
+            delays.append(delay)
+    return delays
